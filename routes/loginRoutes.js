79ac6a11dc9cc382961ab56de9e99fbd6b168367
Vikas Jyani <99635465+vickclouwood@users.noreply.github.com>
2022-05-16 15:56:20 +0530

    added mongodb find query.

--- routes/loginRoutes.js
+++ routes/loginRoutes.js
@@ -1,12 +1,24 @@
 const express = require("express");
+const { Collection } = require("mongoose");
 const router = express.Router();
 const { v4: uuidv4 } = require("uuid");
+const { default: mongoose } = require("mongoose");
+
+const mongoString = process.env.DATABASE_URL;
+
+mongoose.connect(mongoString);
+const database = mongoose.connection;
 
 router.post("/", (req, res) => {
   //res.status(200).json({ message: "Set goals" });
   let email = req.body.email;
   let id = req.body.id;
   //Mongo Select with email and id
+
+  // write down a mongobdb find query
+  const document = database
+    .collection("users")
+    .findOne({ email: email, id: id });
 
   if (document) {
     const seed = uuidv4();
